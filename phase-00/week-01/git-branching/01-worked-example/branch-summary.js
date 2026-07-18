77e3057b97b8f70df147aa15bc89dd05258bb6e1
thesinanouri <thesinanouri@gmail.com>
2026-07-18 11:20:22 +0330

    Update branching summary

--- phase-00/week-01/git-branching/01-worked-example/branch-summary.js
+++ phase-00/week-01/git-branching/01-worked-example/branch-summary.js
@@ -1,3 +1,5 @@
-const currentBranchTopic = "Creating a branch";
+const currentBranchTopic = "Creating and merging a branch";
+const branchName = "practice/update-branch-summary";
 
 console.log(`Current topic: ${currentBranchTopic}`);
+console.log(`Branch: ${branchName}`);
